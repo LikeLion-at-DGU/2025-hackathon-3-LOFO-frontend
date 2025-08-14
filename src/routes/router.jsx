9b96d6eb7766dcf0beded5676396f4c5358e98b8
leeboyeon17 <2023110695@dgu.ac.kr>
2025-08-15 02:50:21 +0900

    feat: 라우팅 경로 수정

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
 import Onboarding from "../pages/Onboarding/Onboarding";
-import SignIn from "../pages/SignIn/SignIn";
+import SignUp from "../pages/SignUp/SignUp";
 
 
 const router = createBrowserRouter([
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     path: "/", // 루트 경로
     element: <App />,
     children: [{ path: "/", element: <Onboarding /> },
-               { path: "/detail/", element: <SignIn /> }
+               { path: "/signup/", element: <SignUp /> }
             ],
   },
 ]);
