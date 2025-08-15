f1eb1fb1440404353a5343ea6293cbcfb398c0f5
leeboyeon17 <2023110695@dgu.ac.kr>
2025-08-16 02:31:14 +0900

    chore: 아웃라인 제거

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { Outlet } from "react-router-dom";
 import './App.css'
 
 const Wrapper = styled.div`
-  border: 1px solid black;
+  //border: 1px solid black;
   width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  * {border: 1px solid black;}
+  //* {border: 1px solid black;}
 `;
 
 // named export
