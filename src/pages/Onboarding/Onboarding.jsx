2b2d86cbcb832faee4a0c32574d5bfcfdc143c27
leeboyeon17 <2023110695@dgu.ac.kr>
2025-08-16 23:48:09 +0900

    feat: 온보딩5 UI 구현

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -3,6 +3,7 @@ import Scroll1 from "../Onboarding/components/Scroll1";
 import Scroll2 from "../Onboarding/components/Scroll2";
 import Scroll3 from "../Onboarding/components/Scroll3";
 import Scroll4 from "../Onboarding/components/Scroll4";
+import Scroll5 from "../Onboarding/components/Scroll5";
 
 const Container = styled.div`
   height: 500vh; //100vh?
@@ -39,7 +40,7 @@ function Onboarding() {
       <Scroll2/>
       <Scroll3/>
       <Scroll4/>
-      <Section style={{ background: "#4156b6ff" }}>Page 5</Section>
+      <Scroll5/>
     </Container>
     </>
   );
