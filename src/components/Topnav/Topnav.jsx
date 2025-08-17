be1f6dcd1ccac60094602860251d1f61563a762a
leeboyeon17 <2023110695@dgu.ac.kr>
2025-08-17 22:19:14 +0900

    feat: Topnav 컴포넌트 구현

---
+++ src/components/Topnav/Topnav.jsx
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+import logo from "../../assets/logo.svg";
+
+const Nav = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 88px;
+    padding: 29px 32px;
+    align-items: center;
+    border-bottom: 1px solid var(--line-001, #BABABA);
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    background: var(--BG-001, #F9FAFB);
+`
+
+function Topnav() {
+  return (
+    <Nav>
+      <img src={logo} alt="로고"/>
+    </Nav>
+  );
+};
+
+export default Topnav;
