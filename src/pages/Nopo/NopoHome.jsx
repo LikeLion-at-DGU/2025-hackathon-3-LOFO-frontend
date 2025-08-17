15b58401a66dafbc783a45224a02c5c9a19c888b
HongYounKim <lunakby1001@gmail.com>
2025-08-17 23:27:00 +0900

    pull전 push

--- src/pages/Nopo/NopoHome.jsx
+++ src/pages/Nopo/NopoHome.jsx
@@ -4,17 +4,17 @@ import Scroll1 from "./NopoHome/components/Scroll1";
 import Scroll2 from "./NopoHome/components/Scroll2";
 import logo from "../../assets/logo.svg";
 
-const Top_nav = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 88px;
-  padding: 16px 8px;
-  border-bottom: 1px solid var(--line-001, #bababa);
-  position: absolute;
-  /* top: 0;
-    left: 0; */
-`;
+// const Top_nav = styled.div`
+//   display: flex;
+//   align-items: center;
+//   width: 100vw;
+//   height: 88px;
+//   padding: 16px 8px;
+//   border-bottom: 1px solid var(--line-001, #bababa);
+//   position: absolute;
+//   /* top: 0;
+//     left: 0; */
+// `;
 
 const Container = styled.div`
   height: 500vh; //100vh?
@@ -31,9 +31,9 @@ const NopoHome = () => {
   return (
     <>
       <Container>
-        <Top_nav>
+        {/* <Top_nav>
           <img src={logo} alt="로고" />
-        </Top_nav>
+        </Top_nav> */}
         <Scroll1 />
         <Scroll2 />
       </Container>
--- src/pages/Nopo/NopoHome/components/Scroll1.jsx
+++ src/pages/Nopo/NopoHome/components/Scroll1.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../../../../assets/logo.svg";
 
 const Wrapper = styled.div`
+  width: 1920px;
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -9,6 +11,18 @@ const Wrapper = styled.div`
   align-items: center;
   text-align: center;
   background: #fff;
+`;
+
+const Top_nav = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 88px;
+  padding: 16px 8px;
+  border-bottom: 1px solid var(--line-001, #bababa);
+  position: absolute;
+  /* top: 0;
+    left: 0; */
 `;
 
 const Title = styled.h1`
@@ -36,6 +50,9 @@ const Button = styled.button`
 const Scroll1 = () => {
   return (
     <Wrapper>
+      <Top_nav>
+        <img src={logo} alt="로고" />
+      </Top_nav>
       <Title>내 가게를 더욱 빛내줄 청년들의 작업물을 선택해보세요</Title>
       <Subtitle>내 가게에 대한 요청을 입력하고 도움을 얻을 수 있어요</Subtitle>
       <Button>요청 쓰러가기</Button>
