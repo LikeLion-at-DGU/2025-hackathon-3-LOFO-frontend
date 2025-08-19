f6bcc8a932592e80b989eae17841f7c341198232
HongYounKim <lunakby1001@gmail.com>
2025-08-19 14:45:07 +0900

    feat/#31: 로고 파일 업로드, link 수정

--- src/pages/Lofo/Community.jsx
+++ src/pages/Lofo/Community.jsx
@@ -1,22 +1,16 @@
 import styled from "styled-components";
-import * as S from "../components/Styled";
-import NopoTopnav from "../../../components/Topnav/NopoTopnav";
-import { HeadingContainer, Title, Subtitle } from "../components/Heading";
+import * as S from "../Nopo/components/Styled";
+import NopoTopnav from "../../components/Topnav/NopoTopnav";
+import { HeadingContainer, Title, Subtitle } from "../Nopo/components/Heading";
 
 const Community = () => {
   return (
     <S.Wrapper>
       <NopoTopnav />
-      <HeadingContainer>
-        <Title>내 가게에 0명이 참여 중이에요</Title>
-        <Subtitle>조금만 기다리면 작업물이 도착할 거예요!</Subtitle>
-      </HeadingContainer>
-
-      <ReceivedContainer>
-        <Subtitle style={{ color: "#E19543" }}>
-          내 가게를 도와준 청년들의 작업물 보기
-        </Subtitle>
-      </ReceivedContainer>
+      <S.HeadingContainer>
+        <S.Title>내 가게에 0명이 참여 중이에요</S.Title>
+        <S.Subtitle>조금만 기다리면 작업물이 도착할 거예요!</S.Subtitle>
+      </S.HeadingContainer>
     </S.Wrapper>
   );
 };
